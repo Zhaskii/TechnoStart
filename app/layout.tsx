fadc0977714e1381203a3ede44c4f4c1f71717b0
ArkshGroup <webdeveloper@arkshgroup.com>
2026-06-18 17:10:19 +0545

    font changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,16 +6,6 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import BackToTop from "./components/BackToTop";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
   title: "Techno Star | Web Development & Digital Marketing Company in Nepal",
   description:
@@ -57,14 +47,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <Navbar />
-      <body className="min-h-full flex flex-col">{children}</body>
-      <BackToTop />
-      <Footer />
+    <html lang="en" className={`h-full antialiased`}>
+      <body className="min-h-full flex flex-col">
+        <Navbar />
+        {children}
+        <BackToTop />
+        <Footer />
+      </body>
     </html>
   );
 }
